553b0f89e18a65f66a7a9aaa101f617ad34eb2c6
Renato Silva <reeh.cr2@gmail.com>
2020-11-23 21:59:00 -0300

    add Pagina do mapa

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 import './styles/global.css'
 
+import Routes from './routes'
+
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Renato</h1>
-    </div>
+    <Routes></Routes>
   );
 }
 
