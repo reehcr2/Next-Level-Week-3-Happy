7411852b1c9cf11d360afc51a4aad8d27ae96d95
reehcr2 <46761524+reehcr2@users.noreply.github.com>
2021-02-08 22:54:02 -0300

    add migrations

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 
+import './database/connection'
+
 const app = express();
 
 app.use(express.json());
 
 app.get('/users', (request, response) => {
-    return response.json({message: 'To funcionando!'})
+    return response.json({message: 'To funcionando de boas!'})
 })
 
 app.listen(3333)
